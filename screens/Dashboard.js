ef0c200e91e272d50a5ca3d25a0d84cec2c5923f
momiah <moemia12@gmail.com>
2024-02-12 21:52:09 +0000

    animated pulse on new order

--- screens/Dashboard.js
+++ screens/Dashboard.js
@@ -1,6 +1,6 @@
 // DashboardNav.js
 import React, { useState, useEffect } from "react";
-import { TouchableOpacity, Text } from "react-native";
+import { TouchableOpacity, View, Text, Animated, Easing } from 'react-native';
 import styled from "styled-components/native";
 import DashboardNavConfig from "../components/DashboardNav/DashboardNav.config";
 import AllOrders from "../components/AllOrders";
@@ -12,6 +12,39 @@ const Dashboard = ({ navigation }) => {
   const [orders, setOrders] = useState([]);
   const [filter, setFilter] = useState('allOrders');
   const [showPopup, setShowPopup] = useState(false);
+  const pulseValue = new Animated.Value(1);
+
+  useEffect(() => {
+    let animation = Animated.loop(
+      Animated.sequence([
+        Animated.timing(pulseValue, {
+          toValue: 1.015,
+          duration: 500,
+          easing: Easing.linear,
+          useNativeDriver: true
+        }),
+        Animated.timing(pulseValue, {
+          toValue: 1,
+          duration: 500,
+          easing: Easing.linear,
+          useNativeDriver: true
+        })
+      ]),
+      { iterations: -1 } // -1 indicates infinite iterations
+    );
+  
+    if (showPopup) {
+      animation.start();
+    } else {
+      animation.stop();
+    }
+  
+    return () => {
+      animation.stop();
+    };
+  }, [showPopup]);
+  
+  
 
   const filterFunc = (order) => {
     const temp = ["liveOrders", "completedOrders", "stats", "allOrders"]
@@ -60,13 +93,15 @@ const Dashboard = ({ navigation }) => {
 
   return (
     <DashboardContainer>
-       {showPopup && (
-        <PopupContainer onPress={handlePopupTap}>
-          <PopupContent>
+      {showPopup && (
+        <Animated.View style={[styles.popupContainer, { transform: [{ scale: pulseValue }] }]}>
+          <TouchableOpacity onPress={handlePopupTap}>
+            <View style={styles.popupContent}>
             <Text style={{fontSize: 50, color: 'white'}}>New Order Received!</Text>
             <Text style={{fontSize: 20, color: 'white'}}>Tap to close</Text>
-          </PopupContent>
-        </PopupContainer>
+            </View>
+          </TouchableOpacity>
+        </Animated.View>
       )}
       <NavContainer>
         {DashboardNavConfig.map((item) => (
@@ -82,6 +117,7 @@ const Dashboard = ({ navigation }) => {
   );
 };
 
+
 const NavContainer = styled.View({
   flexDirection: "column",
   justifyContent: "flex-start",
@@ -96,16 +132,35 @@ const DashboardContainer = styled.View({
   flex: 1
 })
 
-const PopupContainer = styled.TouchableOpacity({
-  position: 'absolute',
- width: '90%',
- height: '90%',
- left: '5%',
-  backgroundColor: 'rgba(0, 30, 0, 0.9)',
-  padding: '20px',
-  borderRadius: '10px',
-  zIndex: 10
-})
+const styles = {
+  popupContainer: {
+    position: 'absolute',
+    width: '90%',
+    height: '90%',
+    left: '5%',
+     backgroundColor: 'rgba(0, 30, 0, 0.9)',
+     padding: '20px',
+     borderRadius: '10px',
+     zIndex: 10
+  },
+  popupContent: {
+    display: 'flex',
+  height: '100%',
+  alignItems: 'center',
+  justifyContent: 'center'
+  }
+};
+
+// const PopupContainer = styled.TouchableOpacity({
+//   position: 'absolute',
+//  width: '90%',
+//  height: '90%',
+//  left: '5%',
+//   backgroundColor: 'rgba(0, 30, 0, 0.9)',
+//   padding: '20px',
+//   borderRadius: '10px',
+//   zIndex: 10
+// })
 
 const PopupContent = styled.View({
   display: 'flex',
